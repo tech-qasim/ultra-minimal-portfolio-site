8a1ac73f9b43aa238494647c5662a41cbaea3acd
Qasim <qasimzexal@gmail.com>
2025-11-22 22:26:06 +0500

    fixed some things.

---
+++ postcss.config.mjs
@@ -0,0 +1,8 @@
+// postcss.config.mjs
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}, // Often included with Tailwind CSS
+    // Other PostCSS plugins can be added here
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "/ultra-minimal-portfolio-site/",  // ⬅️ VERY IMPORTANT FOR GITHUB PAGES
+    base: "./",  // ⬅️ VERY IMPORTANT FOR GITHUB PAGES
 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
 
-    plugins: [react()],
+    plugins: [react(), tailwindcss(),],
 
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
